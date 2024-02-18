de56367c3d7f8f29f9efb104289c54f6f7f490df
jiantosca <jiantosca@gmail.com>
2024-02-17 21:57:08 -0500

    initial commit

---
+++ src/components/RequestBuilder.tsx
@@ -0,0 +1,18 @@
+import { Paper, Stack, Button, Menu, MenuItem} from '@mui/material'
+import KeyboardArrowDownIcon  from '@mui/icons-material/KeyboardArrowDown'
+
+export const RequestBuilder = () => {
+  return (
+    <Paper 
+      sx={{padding: '22px', margin: '0px 15px 15px 15px'}}
+      elevation={4}>
+      This is some shitXXX...
+    </Paper>
+  )
+}
+
+const MethodDropDown = () => {
+  return (
+    <></>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  //StrictMode causes some re-rerendering and multiple comp calls which i want to avoid
+  // since it's confusing while i dubug my own code
+  // maybe enable it later
+  // https://react.dev/reference/react/StrictMode
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
